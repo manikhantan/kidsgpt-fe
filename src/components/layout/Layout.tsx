d380c8391208f63a1f84145a0313535513860127
mani <manikhantan@outlook.com>
2025-11-17 02:10:08 +0530

    rev

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,14 +10,14 @@ const Layout = ({ children }: LayoutProps) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-gray-50">
       <Header
         showMenuButton={true}
         onMenuToggle={() => setSidebarOpen(!sidebarOpen)}
       />
-      <div className="flex flex-1">
+      <div className="flex">
         <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-        <main className="flex-1 p-6 flex flex-col">{children}</main>
+        <main className="flex-1 p-6">{children}</main>
       </div>
     </div>
   );
--- src/components/parent/ParentChatInterface.tsx
+++ src/components/parent/ParentChatInterface.tsx
@@ -53,7 +53,7 @@ const ParentChatInterface = () => {
   };
 
   return (
-    <div className="flex flex-col flex-1 bg-gray-50">
+    <div className="flex flex-col h-full bg-gray-50">
       <div className="flex-1 flex flex-col min-h-0">
         <MessageList messages={messages} />
 
